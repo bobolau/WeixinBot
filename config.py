4a99b78dd33b5a5a4d7fa7fdbfa03e081f7b0b0b
bobo <bobolau@live.com>
2017-01-09 00:16:19 +0800

    weixinRobot init

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+configs = {
+    'WeixinRobot': {
+    },
+    'WebWeixin':{
+        'DEBUG': True,
+        'interactive': False,
+        'autoReplyMode':True,
+        'user_agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36',
+        'autoOpen': False
+    },
+    'MessageRobot': {
+        'simsimi_key': 'ec15bb3e9e524e46868bc3b7c5a9cde3',
+        'turing_key': 'ec15bb3e9e524e46868bc3b7c5a9cde3',
+        'xiaodoubi_key': 'ec15bb3e9e524e46868bc3b7c5a9cde3'
+    },
+    'preload-data': {
+    }
+}
+
